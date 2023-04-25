fb12f31fde2d5cd14b6455796c309b2b51d8bcbe
krgarav <krgarav@gmail.com>
2023-04-25 09:44:16 +0530

    login test added

--- src/Component/Login.test.js
+++ src/Component/Login.test.js
@@ -1,15 +1,26 @@
 import { render, screen } from "@testing-library/react";
+import { expect, jest, test } from "@jest/globals";
 import Login from "./Login";
+import {BrowserRouter} from "react-router-dom"
 import "@testing-library/jest-dom/extend-expect";
 describe("Login component", () => {
   test("Testing Login", () => {
-    render(<Login />);
+    render(<BrowserRouter><Login /></BrowserRouter>);
     const linkElement = screen.getByText("Email address");
     expect(linkElement).toBeInTheDocument();
   });
   test("Testing forgot your password", () => {
-    render(<Login/>)
+    render(<BrowserRouter><Login /></BrowserRouter>);
     const linkElement = screen.getByText("Forgot your password?");
     expect(linkElement).toBeInTheDocument();
+  });
+  test("async testing", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async()=>[{idToken:"hgfhfhyu"}]
+    })
+    render(<BrowserRouter><Login /></BrowserRouter>);
+    const elm = await screen.findAllByText("Welcome to Mailbox");
+    expect(elm).toBeInTheDocument();
   });
 });
